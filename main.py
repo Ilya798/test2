74da84b6fe1fc0f13fc744aedc4f6de4e3371da8
Admin <vysotskijiv@yandexlyceum.ru>
2022-12-10 18:44:48 +0300

    ver 4.nb1

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 def my_superfunction():
-    print('What an awesome print!')
+    print('hello!')
 
 
 def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO c ame a nd added this lin')
+
     my_superfunction()
 
 
